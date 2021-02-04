f867e38867ca2af1de19e5145c22ba392f1b6501
Peter Gammelgaard Poulsen <peter.gammelgaard@gmail.com>
2021-02-04 20:19:10 +0100

    Added townlong-yak

---
+++ townlong_yak.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+if [ $# -eq 0 ]
+  then
+    echo "Usage: townlong_yak output_file"
+    exit 1
+fi
+
+endpoint="https://hub.wowup.io/addons/author/foxlit"
+addons=$(curl -s $endpoint | jq -c \
+  '.addons | map(
+  (if (.releases | length) > 0 then .releases | group_by(.game_type) else null end) as $releases |
+    {
+    id: .id,
+    websiteUrl: .repository,
+    dateReleased: (if (.releases | length) > 0 then .releases[0].published_at else null end),
+    name: .repository_name,
+    summary: .description,
+    numberOfDownloads: .total_download_count,
+    categories: [],
+    flavors: $releases | map( "wow_" + .[0].game_type ),
+    gameVersions: $releases | map({ flavor: ("wow_" + .[0].game_type), gameVersion: .[0].game_version }),
+    source: "townlong-yak"
+  })')
+if [ $(echo $addons | jq 'length') -eq "0" ]; then
+  echo "Error: Found 0 townlong-yak addons"
+  exit 1;
+fi
+
+echo $addons > $1
