e2763c6d72535bc3eb9996a154bdd28532cfb5ea
Casper Rogild Storm <2248455+casperstorm@users.noreply.github.com>
2021-05-19 21:17:38 +0200

    removed appended wow string to flavor

--- townlong_yak.sh
+++ townlong_yak.sh
@@ -17,7 +17,7 @@ addons=$(curl -s $endpoint | jq -c \
     summary: .description | sub("<[^>]*>"; ""; "g"),
     numberOfDownloads: .total_download_count,
     categories: [],
-    gameVersions: $releases | map({ flavor: ("wow_" + .[0].game_versions[0].game_type), gameVersion: .[0].game_versions[0].interface }),
+    gameVersions: $releases | map({ flavor: .[0].game_versions[0].game_type, gameVersion: .[0].game_versions[0].interface }),
     source: "townlong-yak"
   })')
 if [ $(echo $addons | jq 'length') -eq "0" ]; then
