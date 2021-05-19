d2b90630f989e998337e4aa38c016568bf25f0cb
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-05-19 15:38:53 +0200

    updated tukui

--- tukui.sh
+++ tukui.sh
@@ -8,6 +8,7 @@ fi
 endpoint="https://www.tukui.org/api.php"
 retail_endpoint="$endpoint?addons=all"
 classic_endpoint="$endpoint?classic-addons=all"
+tbc_endpoint="$endpoint?classic-tbc-addons=all"
 elvui_endpoint="$endpoint?ui=elvui"
 tukui_enspoint="$endpoint?ui=tukui"
 tmp=$(mktemp -d -t ci-XXXXXXXXXX)
@@ -16,11 +17,12 @@ classic=$tmp/classic.json
 elvui=$tmp/elvui.json
 tukui=$tmp/tukui.json
 all=$tmp/all.json
-curl -s $retail_endpoint | jq '[.[] | . + { "flavors": ["wow_retail"], "gameVersions": [{"flavor": "wow_retail", "gameVersion": (if (.patch == null) then "" else .patch end) }] }]' > $retail
-curl -s $classic_endpoint | jq '[.[] | . + { "flavors": ["wow_classic"], "gameVersions": [{"flavor": "wow_classic", "gameVersion": (if (.patch == null) then "" else .patch end) }] }]' > $classic
-curl -s $elvui_endpoint | jq '[. | . + { "flavors": ["wow_retail"], "gameVersions": [{"flavor": "wow_retail", "gameVersion": (if (.patch == null) then "" else .patch end) }] }]' > $elvui
-curl -s $tukui_enspoint | jq '[. | . + { "flavors": ["wow_retail"], "gameVersions": [{"flavor": "wow_retail", "gameVersion": (if (.patch == null) then "" else .patch end) }] }]' > $tukui
-jq -s add $retail $classic $elvui $tukui > $all
+curl -s $retail_endpoint | jq '[.[] | . + { "gameVersions": [{"flavor": "wow_retail", "gameVersion": (if (.patch == null) then "" else .patch end) }] }]' > $retail
+curl -s $classic_endpoint | jq '[.[] | . + { "gameVersions": [{"flavor": "wow_classic", "gameVersion": (if (.patch == null) then "" else .patch end) }] }]' > $classic
+curl -s $tbc_endpoint | jq '[.[] | . + { "gameVersions": [{"flavor": "wow_burning_crusade", "gameVersion": (if (.patch == null) then "" else .patch end) }] }]' > $tbc
+curl -s $elvui_endpoint | jq '[. | . + { "gameVersions": [{"flavor": "wow_retail", "gameVersion": (if (.patch == null) then "" else .patch end) }] }]' > $elvui
+curl -s $tukui_enspoint | jq '[. | . + { "gameVersions": [{"flavor": "wow_retail", "gameVersion": (if (.patch == null) then "" else .patch end) }] }]' > $tukui
+jq -s add $retail $classic $tbc $elvui $tukui > $all
 if [ $(jq 'length' $all) -eq "0" ]; then
   echo "Error: Found 0 tukui addons"
   exit 1;
@@ -35,7 +37,6 @@ jq -c \
     "summary": .small_desc|gsub("[\\r\\n\\t]"; ""),
     "numberOfDownloads": .downloads|tonumber,
     "categories": (if (.category == null) then [] else [.category] end),
-    "flavors": .flavors,
     "gameVersions": .gameVersions,
     "source": "tukui"
   })' $all > $1
